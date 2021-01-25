f6efe5470d95bfc7c21f56b54e3c5f2039a6f473
Rishav78 <rishavgarg789@gmail.com>
2021-01-26 00:13:29 +0530

    basic setup

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export interface SigninProps {
+  a?: string;
+}
+
+export const Signin = ({}: SigninProps) => {
+  return (
+    <View>
+      <Text>z</Text>
+    </View>
+  );
+};
