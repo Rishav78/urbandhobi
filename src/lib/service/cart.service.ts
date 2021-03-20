1c80d53427b66379be1fedbe2eea13fcad06f303
Rishav78 <rishavgarg789@gmail.com>
2021-03-21 00:16:19 +0530

    implemented services

--- src/lib/service/cart.service.ts
+++ src/lib/service/cart.service.ts
@@ -1,43 +1,71 @@
-import { Cart } from "@urbandhobi/@types";
+import { AddItemBody, Cart, CartItem, CartNItems, GenericObject } from "@urbandhobi/@types";
 import { api } from "../config";
 import { getTokens } from "../helpers";
-import { getFetchWrapper } from "../utils";
+import { getFetchWrapper, Iterator } from "@urbandhobi/lib/utils";
 
 export class CartService {
 
-  private cartId: string;
-  private itemId?: string;
+  constructor(
+    private _cart?: string,
+    private _item?: string
+  ) { }
 
-  constructor(cartId: string, itemId?: string) {
-    this.cartId = cartId;
-    this.itemId = itemId;
+  public cart = (cart: string) => {
+    this._cart = cart;
+    return this;
+  }
+
+  public item = (item: string) => {
+    this._item = item;
+    return this;
   }
 
   public createCart = async () => {
-    const url = api.cart.addItem(this.cartId);
+    // const url = api.cart.addItem(this.cartId);
   }
 
-  public getCarts = async () => {
-    const url = api.cart.getCart(this.cartId);
+  public getCart = async (): Promise<{
+    items: GenericObject<CartItem[]>;
+    cart: Cart;
+  } | undefined> => {
+    const url = api.cart.getCart(this._cart);
     try {
-      const {auth} = await getTokens();
-      const [cart] = await getFetchWrapper<null, Cart[]>(url, "GET")
+      const { auth } = await getTokens();
+      const res = await getFetchWrapper<null, CartNItems[]>(url, "GET")
         .setTokens(auth)
         .send();
-      return cart;
+      if (res) {
+        const { items, ...cart } = res[0];
+        const mapItems = await Iterator.group(items, item => item.cleanType);
+        return { items: mapItems, cart };
+      }
     }
     catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 
-  public deleteCart = () => {}
+  public deleteCart = () => { }
 
-  public addItem = () => {}
+  public addItem = async (data: AddItemBody[]) => {
+    const url = api.cart.addItem(this._cart!);
+    try {
+      const { auth } = await getTokens();
+      await getFetchWrapper(url, "PUT")
+        .setTokens(auth)
+        .setData(data)
+        .send();
+      return true;
+    }
+    catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
 
-  public updateItem = () => {}
+  public updateItem = () => { }
 
-  public deleteItem = () => {}
+  public deleteItem = () => { }
 }
 
 export default CartService;
